9a67bd81e0ba3e00d11289c8342bafdf8716b6ec
Denise Kurek <denisekurek@rocketmail.com>
2020-06-07 19:42:47 -0400

    Button setup
    Got buttons setup and clickable and got basics of script to work.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-a
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d')
+const p1Button = document.querySelector('#start1P');
+const autoButton = document.querySelector('#auto');
+const pauseButton = document.querySelector('#pause');
+const endButton = document.querySelector('#end');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function logs() {
+  console.log('the button was clicked');
+}
+
+p1Button.addEventListener('click', logs);
+autoButton.addEventListener('click', logs);
+pauseButton.addEventListener('click', logs);
+endButton.addEventListener('click', logs);
