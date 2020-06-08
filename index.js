4b0f921b119036db379d168e80c72b0db1374932
Denise Kurek <denisekurek@rocketmail.com>
2020-06-07 22:40:00 -0400

    updates

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const p1Button = document.querySelector('#start1P');
 const autoButton = document.querySelector('#auto');
 const pauseButton = document.querySelector('#pause');
 const endButton = document.querySelector('#end');
+let inGame = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function logs() {
-  console.log('the button was clicked');
-}
+function startGame1P() {
+  console.log('Start a 1 Player game');
+};
 
-p1Button.addEventListener('click', logs);
-autoButton.addEventListener('click', logs);
-pauseButton.addEventListener('click', logs);
-endButton.addEventListener('click', logs);
+function startAutoGame() {
+  console.log('Start a Auto Pong Game')
+};
+
+function pauseGame() {
+  console.log('Pausing Game');
+};
+
+function endGame() {
+  console.log('Ending Game');
+};
+
+p1Button.addEventListener('click', startGame1P);
+autoButton.addEventListener('click', startAutoGame);
+pauseButton.addEventListener('click', pauseGame);
+endButton.addEventListener('click', endGame);
