51e0f0c232b4b575bfabd7f56808a6a9b6c7fa22
Denise Kurek <denisekurek@rocketmail.com>
2020-06-25 22:25:32 -0400

    Commented out pause button and gained the ability to move the left/player paddle with keyboard buttons. Created individual functions to draw objects for the game.

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const pauseButton = document.querySelector('#pause');
 const endButton = document.querySelector('#end');
 const scoreLeft = document.querySelector('#left-score');
 const scoreRight = document.querySelector('#right-score');
+// const pauseAction = document.querySelector('#pause');
 let leftScore = 0;
 let rightScore = 0;
 let inGame = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let deltaY = 0;
 
-scoreLeft.textContent = 'Select Game Mode';
 
 function startGame1P() {
   // console.log('Start a 1 Player game');
+  drawBall();
+  drawLeftPaddle();
+  drawRightPaddle();
   drawBaseGame();
   inGame = true;
   scoreLeft.textContent = leftScore;
@@ -30,12 +34,20 @@ function startAutoGame() {
   scoreRight.textContent = rightScore;
 };
 
-function pauseGame() {
-  // console.log('Pausing Game');
-  inGame = true;
-  scoreLeft.textContent = leftScore;
-  scoreRight.textContent = rightScore;
-};
+// function pauseGame() {
+//   // console.log('Pausing Game');
+//   let count = 0;
+//   if (count % 2 == 0)
+//   {
+//     pauseAction.textContent = 'Resume Game';
+//     count++;
+//   }
+//   else
+//   {
+//     pauseAction.textContent = 'Pause Game';
+//     count++;
+//   }
+// };
 
 function endGame() {
   // console.log('Ending Game');
@@ -44,33 +56,45 @@ function endGame() {
   scoreRight.textContent = '';
 };
 
+function movepaddle(e) {
+  if (inGame && e.keyCode == 38) //Move the paddle down the canvas
+  {
+    deltaY -= 10;
+    e.preventDefault();
+  }
+  else if (inGame == true && e.keyCode == 40) //Move the paddle Up the canvas
+  {
+    deltaY += 10;
+    e.preventDefault();
+  }
+
+  drawLeftPaddle();
+}
+
 p1Button.addEventListener('click', startGame1P);
 autoButton.addEventListener('click', startAutoGame);
-pauseButton.addEventListener('click', pauseGame);
+// pauseButton.addEventListener('click', pauseGame);
 endButton.addEventListener('click', endGame);
 
-document.addEventListener('keydown', (e) => {
-  if (inGame && e.keyCode == 38)
-  {
-    console.log('going up');
-  }
-  else if (inGame == true && e.keyCode == 40)
-  {
-    console.log('going down');
-  }
-});
+document.addEventListener('keydown', movepaddle);
 
 function drawBaseGame() {
-  //Creates the base game on the canvas
   ctx.fillStyle = "#ffffff";
-  //Creates left paddle
-  let paddleLeft = ctx.fillRect(canvas.width *.05, (canvas.height * .5) - 75, 20, 150);
-  //Create right paddle
-  let paddleRight = ctx.fillRect(canvas.width - (canvas.width * .05) - 20, (canvas.height * .5) - 75, 20, 150);
-  //Create center line
   let centerLine = ctx.fillRect(canvas.width * .5 - 2.5, 0, 5, canvas.height);
-  //Draw ball on centerline
+};
+
+function drawLeftPaddle() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  let paddleLeft = ctx.fillRect(canvas.width *.05, (canvas.height * .5) - 75 + deltaY, 20, 150 );
+};
+
+function drawRightPaddle() {
+  let paddleRight = ctx.fillRect(canvas.width - (canvas.width * .05) - 20, (canvas.height * .5) - 75, 20, 150);
+};
+
+function drawBall() {
+  ctx.fillStyle = "#ffffff";
   ctx.beginPath();
   const ball = ctx.arc(canvas.width * .5, canvas.height * .5, canvas.height * .02, 0, 2 * Math.PI);
   ctx.fill();
-}
+};
