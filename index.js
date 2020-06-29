8d155e40f85bf0ac103076aca9d3f0c69406cf77
Denise Kurek <denisekurek@rocketmail.com>
2020-06-29 14:46:27 -0400

    Draws and clears canvas depending on situation that the button is presses.

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ const pauseButton = document.querySelector('#pause');
 const endButton = document.querySelector('#end');
 const scoreLeft = document.querySelector('#left-score');
 const scoreRight = document.querySelector('#right-score');
+const announcement = document.querySelector('#announce');
 // const pauseAction = document.querySelector('#pause');
 let leftScore = 0;
 let rightScore = 0;
 let inGame = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let deltaY = 0;
-
+let playerPaddleY = 0;
 
 function startGame1P() {
   // console.log('Start a 1 Player game');
-  drawBall();
-  drawLeftPaddle();
-  drawRightPaddle();
+  // drawLeftPaddle();
+  // drawRightPaddle();
   drawBaseGame();
+  // drawBall();
   inGame = true;
+  announcement.textContent = '';
   scoreLeft.textContent = leftScore;
   scoreRight.textContent = rightScore;
 };
@@ -30,6 +31,7 @@ function startAutoGame() {
   // console.log('Start a Auto Pong Game');
   inGame = true;
   drawBaseGame();
+  announcement.textContent = '';
   scoreLeft.textContent = leftScore;
   scoreRight.textContent = rightScore;
 };
@@ -55,16 +57,19 @@ function endGame() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   if (leftScore > rightScore)
   {
-    scoreLeft.textContent = 'Player 1 Wins';
+    announcement.textContent = 'Player 1 Wins!'
+    scoreLeft.textContent = '';
     scoreRight.textContent = '';
   }
   else if (rightScore > leftScore)
   {
-    scoreLeft.textContent = 'Computer Wins';
+    announcement.textContent = 'Computer Wins!';
+    scoreLeft.textContent = '';
     scoreRight.textContent = '';
   }
   else {
-    scoreLeft.textContent = 'Draw';
+    announcement.textContent = 'Draw';
+    scoreLeft.textContent = '';
     scoreRight.textContent = '';
   }
 
@@ -73,16 +78,19 @@ function endGame() {
 function movepaddle(e) {
   if (inGame && e.keyCode == 38) //Move the paddle down the canvas
   {
-    deltaY -= 10;
+    playerPaddleY -= 10;
     e.preventDefault();
   }
   else if (inGame == true && e.keyCode == 40) //Move the paddle Up the canvas
   {
-    deltaY += 10;
+    playerPaddleY += 10;
     e.preventDefault();
   }
 
-  drawLeftPaddle();
+  // drawBall();
+  // drawLeftPaddle();
+  // drawRightPaddle();
+  drawBaseGame();
 }
 
 p1Button.addEventListener('click', startGame1P);
@@ -93,22 +101,29 @@ endButton.addEventListener('click', endGame);
 document.addEventListener('keydown', movepaddle);
 
 function drawBaseGame() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
   ctx.fillStyle = "#ffffff";
   let centerLine = ctx.fillRect(canvas.width * .5 - 2.5, 0, 5, canvas.height);
-};
-
-function drawLeftPaddle() {
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-  let paddleLeft = ctx.fillRect(canvas.width *.05, (canvas.height * .5) - 75 + deltaY, 20, 150 );
-};
-
-function drawRightPaddle() {
+  let paddleLeft = ctx.fillRect(canvas.width *.05, (canvas.height * .5) - 75 + playerPaddleY, 20, 150 );
   let paddleRight = ctx.fillRect(canvas.width - (canvas.width * .05) - 20, (canvas.height * .5) - 75, 20, 150);
-};
-
-function drawBall() {
-  ctx.fillStyle = "#ffffff";
   ctx.beginPath();
-  const ball = ctx.arc(canvas.width * .5, canvas.height * .5, canvas.height * .02, 0, 2 * Math.PI);
+  let ball = ctx.arc(canvas.width * .5, canvas.height * .5, canvas.height * .02, 0, 2 * Math.PI);
   ctx.fill();
 };
+
+// function drawLeftPaddle() {
+//   ctx.clearRect(0,0, canvas.width, canvas.height);
+//   let paddleLeft = ctx.fillRect(canvas.width *.05, (canvas.height * .5) - 75 + playerPaddleY, 20, 150 );
+// };
+//
+// function drawRightPaddle() {
+//   let paddleRight = ctx.fillRect(canvas.width - (canvas.width * .05) - 20, (canvas.height * .5) - 75, 20, 150);
+// };
+//
+// function drawBall() {
+//   ctx.fillStyle = "#ffffff";
+//   ctx.beginPath();
+//   let ball = ctx.arc(canvas.width * .5, canvas.height * .5, canvas.height * .02, 0, 2 * Math.PI);
+//   ctx.fill();
+//   // ctx.endPath();
+// };
